0b427e45584d418fb2eff49de7a7bf8382c54940
GumiDung <dungbui@gumiviet.com>
2017-08-10 17:01:10 +0700

    TODO list part 1

--- page-05/js/case.js
+++ page-05/js/case.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
     //Check to see if the window is top if not then display button
+
     $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
             $('.scrollToTop').fadeIn();
